48716bc83e0d907fca490c203dbdb938c4b455dd
Magnus Øverbø <magnus@maggz.nl>
2014-09-17 02:12:24 +0200

    Started the upload script

--- api.py
+++ api.py
@@ -2,6 +2,15 @@
 #-*- coding: utf-8 -*-
 
 import datetime
+import 	sys
+import	MySQLdb
+
+
+HOST		= "127.0.0.1"
+USER		= "root"
+PASSWD	= "toor"
+DB			= "quest"
+
 
 def getSampleText( fname ):
 	filehandle = open( fname, "r" )
@@ -17,4 +26,37 @@ def getSampleText( fname ):
 def getTime( T ):
 	s = datetime.datetime.now() - T
 	return (s.microseconds + s.seconds * (10**6))
+
+
+def opendb():
+	try:
+		CON = MySQLdb.connect( HOST, USER, PASSWD, DB, charset='utf8', use_unicode=True)
+		return CON
+	except:
+		print "ERROR: COULD NOT ESTABLISH CONNECTION TO DATABASE"
+		sys.exit()
+
+def execute( CON, QUERY, DATA  ):
+	CRSR = CON.cursor()
+	try:
+		if DATA is not None:
+			CRSR.execute( QUERY, (DATA) )
+		else:
+			CRSR.execute( QUERY )
+		
+		if "INSERT" in QUERY:
+			QUERY = "SELECT LAST_INSERT_ID()"
+			CRSR.execute( QUERY )
+			
+		CON.commit()
+
+		if "SELECT" in QUERY:
+			RES = CRSR.fetchall()
+			return RES
+		return None;
+	except MySQLdb.Error, e:
+		CON.rollback()
+		print e
+		print "ERROR: FAILED TO EXECUTE QUERY"
+		return None;
 
---
+++ upload.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import 	os
+import 	sys
+from		api			import *
+import	MySQLdb
+
+if len(sys.argv) < 2:
+	print "Jackson\n"
+	sys.exit()
+
+for arg in sys.argv[1:]:
+	print arg
+
+
+
